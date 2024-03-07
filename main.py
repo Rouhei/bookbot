c1566ac604f6993d3c3f36a2a56dc327b86efc52
Rouhei <tylerwolfe00@gmail.com>
2024-03-07 13:39:11 +0000

    Split formatting and char odering into two functions

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 def main(): 
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        wc = get_word_count(file_contents)
-        cc = get_char_count(file_contents)
-        format_report("books/frankenstein.txt", wc, cc)
-       
+        report = format_report("books/frankenstein.txt", get_word_count(file_contents), get_char_count(file_contents))
+        print(report)
 
 def get_word_count(text):
     words = text.split()
@@ -22,6 +20,12 @@ def get_char_count(input_text):
 
 def format_report(filename, wordcount, char_count):
     report = f"--- Begin report on {filename} --- \n{wordcount} words found in the document \n"
+    char_list = sort_characters(char_count)
+    for i in char_list:
+        report += f"The letter '{i["char"]}' was found {i["count"]} times \n"
+    return report
+
+def sort_characters(char_count):
     char_list = []
     for c in char_count:
         if c.isalpha():
@@ -30,9 +34,7 @@ def format_report(filename, wordcount, char_count):
             c_dict["count"] = char_count[c]
             char_list.append(c_dict)
     char_list.sort(reverse=True, key=dic_sort)
-    for i in char_list:
-        report += f"The letter '{i["char"]}' was found {i["count"]} times \n"
-    print (report)
+    return char_list
 
 def dic_sort(dict):
     return dict["count"]
